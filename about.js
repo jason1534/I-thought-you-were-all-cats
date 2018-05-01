3c2ff35cfac88dc7773cbfeda581b123daf4d596
rainshoeshoe <h521369@gmail.com>
2018-05-01 20:57:59 +0800

    talk

--- about.js
+++ about.js
@@ -17,8 +17,10 @@ $(document).ready(function(){
         $("#member1").hover(function(){		
 		same1();
                 $("#f1").animate({left: '-55%'},700);
+                $("#intro1").fadeIn(1000);
 
 	},function(){
+                $("#intro1").fadeOut("quick");
                 $("#f1").animate({left: '-110%'},500);
                 same2();
         });  
@@ -26,8 +28,9 @@ $(document).ready(function(){
         $("#member2").hover(function(){		
 		same1();
                 $("#f2").animate({left: '-25%'},700);
-
+                $("#intro2").fadeIn(1000);
 	},function(){
+                $("#intro2").fadeOut("quick");
                 $("#f2").animate({left: '-78%'},500);
                 same2();
         });
@@ -35,8 +38,9 @@ $(document).ready(function(){
         $("#member3").hover(function(){		
 		same1();
                 $("#f3").animate({left: '-46%'},700);
-
+                $("#intro3").fadeIn(1000);
 	},function(){
+                $("#intro3").fadeOut("quick");
                 $("#f3").animate({left: '-110%'},500);
                 same2();
         });  
@@ -44,8 +48,9 @@ $(document).ready(function(){
         $("#member4").hover(function(){		
 		same1();
                 $("#f4").animate({left: '-30%'},700);
-
+                $("#intro4").fadeIn(1000);
 	},function(){
+                $("#intro4").fadeOut("quick");
                 $("#f4").animate({left: '-110%'},500);
                 same2();
         });  
@@ -53,8 +58,9 @@ $(document).ready(function(){
         $("#member5").hover(function(){		
 		same1();
                 $("#f5").animate({left: '-22%'},700);
-
+                $("#intro5").fadeIn(1000);
 	},function(){
+                $("#intro5").fadeOut("quick");
                 $("#f5").animate({left: '-110%'},500);
                 same2();
         });  
@@ -62,8 +68,9 @@ $(document).ready(function(){
         $("#member6").hover(function(){		
 		same1();
                 $("#f6").animate({left: '-25%'},700);
-
+                $("#intro6").fadeIn(1000);
 	},function(){
+                $("#intro6").fadeOut("quick");
                 $("#f6").animate({left: '-110%'},500);
                 same2();
         });  
@@ -71,15 +78,17 @@ $(document).ready(function(){
         $("#member7").hover(function(){		
 		same1();
                 $("#f7").animate({left: '-25%'},700);
-
+                $("#intro7").fadeIn(1000);
 	},function(){
+                $("#intro7").fadeOut("quick");
                 $("#f7").animate({left: '-110%'},500);
                 same2();
         });
         $("#member8").hover(function(){		
 		same1();
-
+                $("#intro8").fadeIn(1000);
 	},function(){
+                $("#intro8").fadeOut("quick");
                 same2();
         });         
        
