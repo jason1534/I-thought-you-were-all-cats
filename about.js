7dd63f02b4b3859f5fb45c79abd3e3e743f999ce
jaso1534 <a22347630@gmail.com>
2018-05-12 21:51:44 +0800

    RPG conversation

--- about.js
+++ about.js
@@ -22,7 +22,7 @@ function same2($intro,$f,$left)
         $(".title").animate({opacity:'1.0'},500);
 }
 
-/*$(document).ready(function(){	
+$(document).ready(function(){	
         $("#member1").hover(function(){		
                 same1("#member1","#f1","#intro1",'-45%');
 	},function(){
@@ -73,72 +73,72 @@ function same2($intro,$f,$left)
                 same2();
         });         
        
-});*/
-
-$(document).ready(function(){
-  $("#member1").hover(function(){
-    $.doTimeout('hover',400,function(){same1("#member1","#f1","#intro1",'-45%');},'hover');
-        }
-  ,function(){
-        same2("#intro1","#f1",'-140%');
-  });
-
-  $("#member2").hover(function(){
-    $.doTimeout('hover',400,function(){same1("#member2","#f2","#intro2",'-15%');},'hover');
-        }
-  ,function(){
-         same2("#intro2","#f2",'-108%',);
-  });
-
-  $("#member3")
-  .hover(function(){
-    $.doTimeout('hover',400,function(){same1("#member3","#f3","#intro3",'-36%');},'hover');
-        }
-  ,function(){
-       same2("#intro3","#f3",'-140%');
-  });
-
-  $("#member4")
-  .hover(function(){
-    $.doTimeout('hover',400,function(){same1("#member4","#f4","#intro4",'-30%');},'hover');
-        }
-  ,function(){
-        same2("#intro4","#f4",'-140%');
-  });
-
-  $("#member5")
-  .hover(function(){
-    $.doTimeout('hover',400,function(){same1("#member5","#f5","#intro5",'-22%');},'hover');
-        }
-  ,function(){
-        same2("#intro5","#f5",'-140%');
-  });
-
-  $("#member6")
-  .hover(function(){
-    $.doTimeout('hover',400,function(){same1("#member6","#f6","#intro6",'-25%');},'hover');
-        }
-  ,function(){
-       same2("#intro6","#f6",'-140%');
-  });
-
-  $("#member7")
-  .hover(function(){
-    $.doTimeout('hover',400,function(){same1("#member7","#f7","#intro7",'-25%');},'hover');
-        }
-  ,function(){
-        same2("#intro7","#f7",'-140%');
-  });
-
-  $("#member8")
-  .hover(function(){
-    $.doTimeout('hover',400,function(){same1();
-        $("#member8").animate({opacity:'1.0'},200);
-        $("#intro8").fadeIn(1000);},'hover');
-        }
-  ,function(){
-        $("#intro8").fadeOut("quick");
-        same2();
-  }); 
-
 });
+
+// $(document).ready(function(){
+//   $("#member1").hover(function(){
+//     $.doTimeout('hover',400,function(){same1("#member1","#f1","#intro1",'-45%');},'hover');
+//         }
+//   ,function(){
+//         same2("#intro1","#f1",'-140%');
+//   });
+
+//   $("#member2").hover(function(){
+//     $.doTimeout('hover',400,function(){same1("#member2","#f2","#intro2",'-15%');},'hover');
+//         }
+//   ,function(){
+//          same2("#intro2","#f2",'-108%',);
+//   });
+
+//   $("#member3")
+//   .hover(function(){
+//     $.doTimeout('hover',400,function(){same1("#member3","#f3","#intro3",'-36%');},'hover');
+//         }
+//   ,function(){
+//        same2("#intro3","#f3",'-140%');
+//   });
+
+//   $("#member4")
+//   .hover(function(){
+//     $.doTimeout('hover',400,function(){same1("#member4","#f4","#intro4",'-30%');},'hover');
+//         }
+//   ,function(){
+//         same2("#intro4","#f4",'-140%');
+//   });
+
+//   $("#member5")
+//   .hover(function(){
+//     $.doTimeout('hover',400,function(){same1("#member5","#f5","#intro5",'-22%');},'hover');
+//         }
+//   ,function(){
+//         same2("#intro5","#f5",'-140%');
+//   });
+
+//   $("#member6")
+//   .hover(function(){
+//     $.doTimeout('hover',400,function(){same1("#member6","#f6","#intro6",'-25%');},'hover');
+//         }
+//   ,function(){
+//        same2("#intro6","#f6",'-140%');
+//   });
+
+//   $("#member7")
+//   .hover(function(){
+//     $.doTimeout('hover',400,function(){same1("#member7","#f7","#intro7",'-25%');},'hover');
+//         }
+//   ,function(){
+//         same2("#intro7","#f7",'-140%');
+//   });
+
+//   $("#member8")
+//   .hover(function(){
+//     $.doTimeout('hover',400,function(){same1();
+//         $("#member8").animate({opacity:'1.0'},200);
+//         $("#intro8").fadeIn(1000);},'hover');
+//         }
+//   ,function(){
+//         $("#intro8").fadeOut("quick");
+//         same2();
+//   }); 
+
+// });
